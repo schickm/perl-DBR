71e57ed53e9a1a4b3f5e49759d8dcc4b370f45eb
Stefan O'Rear <stefanor@cox.net>
2014-05-22 20:24:45 -0700

    populate dbr.indexes

--- lib/DBR/Config/ScanDB.pm
+++ lib/DBR/Config/ScanDB.pm
@@ -62,11 +62,23 @@ sub update_table{
     my ($self, $name, $info) = @_;
 
     my $dbh = $self->{conf_instance}->connect || die "failed to connect to config db";
+    my $version = $self->{conf_instance}->meta_version;
 
     my $table_id = $dbh->dbr_tables->where( schema_id => $self->{schema_id}, name => $name )->next->table_id;
     $table_id ||= $dbh->dbr_tables->insert( schema_id => $self->{schema_id}, name => $name );
 
     my $fieldmap = $dbh->dbr_fields->where( table_id => $table_id )->hashmap_single('name');
+    my %usedfield;
+
+    my %indexmap;
+    my @ex_indices;
+    if ($version >= 2) {
+        $dbh->indexes->where( field_id => [map { $_->field_id } values %$fieldmap] )->each(sub {
+            my $index = shift;
+            push @ex_indices, $index;
+            $indexmap{$index->refinement_of_id || 0}{ $index->field->name }{ $index->prefix_length || 0 }{ $index->is_unique ? 1 : 0 } = $index->id;
+        });
+    }
 
     foreach my $name (sort keys %{$info->{columns}}) {
         my $field = $info->{columns}{$name};
@@ -80,7 +92,7 @@ sub update_table{
             is_pkey     => $field->{is_pkey} || 0,
             data_type   => $typeid,
             max_value   => $field->{max_value} || 0,
-            $self->{conf_instance}->meta_version >= 2 ? (
+            $version >= 2 ? (
                 decimal_digits => $field->{decimal_digits} || 0,
             ):(),
         };
@@ -88,12 +100,34 @@ sub update_table{
         my $record = delete $fieldmap->{$name};
         if ($record) {
             $record->set( %$ref );
+            $usedfield{$name} = $record->field_id;
         } else {
-            $dbh->dbr_fields->insert( table_id => $table_id, name => $name, %$ref );
+            $usedfield{$name} = $dbh->dbr_fields->insert( table_id => $table_id, name => $name, %$ref );
         }
     }
 
     map { $_->delete } values %$fieldmap;
+
+    if ($version >= 2) {
+        my %ix_used;
+        foreach my $index (values %{$info->{indexes}}) {
+            my $index_id;
+            my @parts = @{$index->{parts}};
+            while (@parts) {
+                my $part = shift @parts;
+                my $col  = $part->{column};
+                my $plen = $part->{prefix_length};
+                my $unique = (!@parts && $index->{unique}) ? 1 : 0;
+
+                $index_id = $indexmap{ $index_id || 0 }{ $col }{ $plen || 0 }{ $unique } ||=
+                    $dbh->indexes->insert( field_id => $usedfield{$col}, refinement_of_id => $index_id, prefix_length => $plen, is_unique => $unique );
+
+                $ix_used{$index_id} = 1;
+            }
+        }
+
+        map { $_->delete unless $ix_used{$_->id} } @ex_indices;
+    }
 }
 
 1;
---
+++ t/14_index_scan.t
@@ -0,0 +1,38 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+no warnings 'uninitialized';
+
+$| = 1;
+
+use lib './lib';
+use t::lib::Test;
+use Test::More tests => 12;
+
+my $dbr = setup_schema_ok('indextest', version => 2);
+
+my $dbh = $dbr->connect('dbrconf');
+ok($dbh, 'dbr connect');
+
+is($dbh->indexes->all->count, 9, 'found all indexes and no extras');
+
+my $ix_abc_a = $dbh->indexes->where( 'field.name' => 'a', refinement_of_id => undef, prefix_length => undef, is_unique => 0 )->next;
+ok ($ix_abc_a, 'found index on abc(a)');
+my $ix_abc_b = $dbh->indexes->where( 'field.name' => 'b', refinement_of_id => undef, prefix_length => undef, is_unique => 1 )->next;
+ok ($ix_abc_b, 'found index on abc(b)');
+
+my $ix_def_d   = $dbh->indexes->where( 'field.name' => 'd', refinement_of_id => undef, prefix_length => undef, is_unique => 0 )->next;
+ok $ix_def_d, 'found prefix index on def(d)';
+my $ix_def_g   = $dbh->indexes->where( 'field.name' => 'g', refinement_of_id => undef, prefix_length => undef, is_unique => 0 )->next;
+ok $ix_def_g, 'found prefix index on def(g)';
+my $ix_def_de  = $dbh->indexes->where( 'field.name' => 'e', refinement_of_id => $ix_def_d->id, prefix_length => undef, is_unique => 1 )->next;
+ok $ix_def_de, 'found unique index on def(de)';
+my $ix_def_gf  = $dbh->indexes->where( 'field.name' => 'f', refinement_of_id => $ix_def_g->id, prefix_length => undef, is_unique => 0 )->next;
+ok $ix_def_gf, 'found index on def(g,f(undef))';
+my $ix_def_gh  = $dbh->indexes->where( 'field.name' => 'h', refinement_of_id => $ix_def_g->id, prefix_length => undef, is_unique => 0 )->next;
+ok $ix_def_gh, 'found index on def(g,h(undef))';
+my $ix_def_gd  = $dbh->indexes->where( 'field.name' => 'd', refinement_of_id => $ix_def_g->id, prefix_length => undef, is_unique => 0 )->next;
+ok $ix_def_gd, 'found prefix index on def(gd)';
+my $ix_def_gde = $dbh->indexes->where( 'field.name' => 'e', refinement_of_id => $ix_def_gd->id, prefix_length => undef, is_unique => 0 )->next;
+ok $ix_def_gd, 'found index on def(gde)';
