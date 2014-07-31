1a86f08d95f69f19ad1b7ffd9a2c129d1b2f32a0
Stefan O'Rear <stefanor@cox.net>
2014-07-30 23:25:46 -0700

    simplify, get rid of double query object

--- lib/DBR/Query/Select.pm
+++ lib/DBR/Query/Select.pm
@@ -14,7 +14,78 @@ use Scalar::Util 'weaken';
 
 sub _params    { qw (fields tables where builder limit offset orderby lock quiet_error) }
 sub _reqparams { qw (fields tables) }
-sub _validate_self{ 1 } # If I exist, I'm valid
+sub _validate_self {
+    my ($self) = @_;
+    # this is our chance to do late fixups after all fields are set
+
+    # convert to historical if needed
+    my $sess = $self->{session};
+    if ($self->{session}->query_time_mode) {
+        my @tbls = @{$self->{tables}};
+        croak('joins must be run simulated in time-query mode') if @tbls != 1;
+        my $cdc = $tbls[0]->cdc_type;
+        if ($cdc->{logged}) {
+            $self->_convert_to_historical;
+        }
+    }
+
+    # field set is final, can set the indices now
+    my $lastidx = -1;
+    for (@{$self->{fields}}) {
+        $_->index( ++$lastidx );
+    }
+    $self->{last_idx} = $lastidx;
+
+    1; # not actually validating
+}
+
+sub _convert_to_historical {
+    my ($self) = @_;
+
+    # rebuild the statement to query the log table
+    my $basetable = $self->{tables}->[0];
+    # preserve alias so that we can cheat and reuse the where tree
+    my $logtable = $basetable->cdc_type->{log_table}->clone( alias => $basetable->alias, instance_id => $self->{instance}->guid )
+        or croak('failed to rebind table object');
+
+    $self->{tables} = [$logtable];
+    $self->{is_historical} = 1;
+
+    # we're going to be consistently including log fields in the field array.
+    # what we're not consistent about is that orderby and where are still referencing non-log fields, and DBR::Record::Maker uses the log field names when code expects non-log field names
+    # both of these depend on the common naming, beyond the fact of the common naming being used for matching
+
+    my %include;
+    map { $include{$_->name} = $_ } @{ $self->{fields} };
+    my %logfields;
+    map { $logfields{$_->name} = $_ } @{ $logtable->fields };
+
+    my $nfields = [ map { $logfields{$_->name} } @{ $self->{fields} } ];
+
+    my @newcon = $self->{where} || ();
+
+    for my $fd (values %logfields) {
+        my $name = $fd->name;
+        if (!$include{$name} && ($name eq 'cdc_start_time' || $name eq 'cdc_end_time' || $name eq 'cdc_row_version')) {
+            push @$nfields, $fd;
+        }
+    }
+
+    my $fd;
+    if ($fd = $logfields{cdc_start_time}) {
+        # starts after end of range -> cannot be interested
+        push @newcon, DBR::Query::Part::Compare->new( field => $fd, operator => 'le', value => $fd->makevalue($self->{session}->query_end_time) );
+    }
+
+    if ($fd = $logfields{cdc_end_time}) {
+        # must end after the first second we care about
+        push @newcon, DBR::Query::Part::Compare->new( field => $fd, operator => 'gt', value => $fd->makevalue($self->{session}->query_start_time) );
+    }
+
+    $self->{splitfield} = $self->{splitfield} && $logfields{$self->{splitfield}->name};
+    $self->{fields} = $nfields;
+    $self->{where} = @newcon ? DBR::Query::Part::And->new( @newcon ) : undef;
+}
 
 sub fields{
       my $self = shift;
@@ -23,12 +94,9 @@ sub fields{
       my @fields = $self->_arrayify(@_);
       scalar(@fields) || croak('must provide at least one field');
 
-      my $lastidx = -1;
       for (@fields){
 	    ref($_) =~ /^DBR::Config::Field/ || croak('must specify field as a DBR::Config::Field object'); # Could also be ::Anon
-	    $_->index( ++$lastidx );
       }
-      $self->{last_idx} = $lastidx;
       $self->{fields}   = \@fields;
 
       return 1;
@@ -49,94 +117,34 @@ sub sql{
           $sql .= ' ORDER BY ' . join(', ', map { $_->sql($conn) } @{ $self->{orderby} || [] });
       }
       $sql .= ' FOR UPDATE'                          if $self->{lock} && $conn->can_lock;
-      $sql .= ' LIMIT ' . $self->_limit_clause       if $self->{limit} || $self->{offset};
+      $sql .= ' LIMIT ' . $self->_limit_clause       if ($self->{limit} || $self->{offset}) && !$self->{is_historical};
 
       $self->_logDebug2( $sql );
       return $sql;
 }
 
+sub is_historical { $_[0]{is_historical} }
 sub lastidx  { $_[0]{last_idx} }
 sub can_be_subquery { scalar( @{ $_[0]->fields || [] } ) == 1 }; # Must have exactly one field
 
 sub run {
       my $self = shift;
-      croak('DBR::Query::Select::run is not usable in time query mode') if $self->{session}->query_time_mode;
+      croak('DBR::Query::Select::run is not usable on historical queries') if $self->{is_historical};
       return $self->{sth} ||= $self->instance->getconn->prepare( $self->sql ) || confess "Failed to prepare"; # only run once
 }
 
-# use this if you will take either a sth or an arrayref
+# use this if you're taking an arrayref and want to support point-in-time queries (which cannot be reduced to a sth)
 sub _exec {
     my $self = shift;
 
-    if ($self->{session}->query_time_mode) {
-        my @tbls = @{$self->{tables}};
-        croak('joins must be run simulated in time-query mode') if @tbls != 1;
-        my $cdc = $tbls[0]->cdc_type;
-        if (!$cdc->{logged}) {
-            # assume this table is permanently valid
-            my $sth = $self->{sth} ||= $self->instance->getconn->prepare( $self->sql ) || confess "Failed to prepare"; # only run once
-            return ($self, $sth, undef);
-        }
-
-        my $pretender = $self->_create_pretender_query($tbls[0]);
-
-        return ($pretender, undef, $pretender->_historical_fetch($self));
+    if ($self->{is_historical}) {
+        return $self->_historical_fetch;
     } else {
         my $sth = $self->{sth} ||= $self->instance->getconn->prepare( $self->sql ) || confess "Failed to prepare"; # only run once
-        return ($self, $sth, undef);
+        defined( $sth->execute ) or croak 'failed to execute statement (' . $sth->errstr. ')';
+        return $sth->fetchall_arrayref or croak 'failed to execute statement (' . $sth->errstr. ')';
     }
 }
-
-sub _create_pretender_query {
-    my ($self) = @_;
-
-    # rebuild the statement to query the log table
-    my $newtable = $self->{tables}->[0]->cdc_type->{log_table}->clone( alias => $self->{tables}->[0]->alias, instance_id => $self->{instance}->guid )
-        or croak('failed to rebind table object');
-
-    # we're going to be consistently including log fields in the field array.
-    # what we're not consistent about is that orderby and where are still referencing non-log fields, and DBR::Record::Maker uses the log field names when code expects non-log field names
-    # both of these depend on the common naming, beyond the fact of the common naming being used for matching
-
-    my %include;
-    map { $include{$_->name} = $_ } @{ $self->{fields} };
-
-    my %index;
-    my @pk_indices;
-    my @newcon = $self->{where} || ();
-    my $newfields = [];
-
-    for my $fd (@{$newtable->fields}) {
-        my $name = $fd->name;
-        if ($name eq 'cdc_start_time' || $name eq 'cdc_end_time' || $name eq 'cdc_row_version' || $include{$name}) {
-            push @$newfields, $fd;
-        }
-        if ($name eq 'cdc_start_time') { # starts after end of range -> cannot be interested
-            push @newcon, DBR::Query::Part::Compare->new( field => $fd, operator => 'le', value => $fd->makevalue($self->{session}->query_end_time) );
-        }
-        if ($name eq 'cdc_end_time') { # must end after the first second we care about
-            push @newcon, DBR::Query::Part::Compare->new( field => $fd, operator => 'gt', value => $fd->makevalue($self->{session}->query_start_time) );
-        }
-    }
-
-    my $new = DBR::Query::Select->new(
-        tables     => [$newtable],
-        fields     => $newfields,
-        where      => @newcon ? DBR::Query::Part::And->new( @newcon ) : undef,
-        orderby    => $self->orderby,
-        instance   => $self->{instance},
-        session    => $self->{session},
-        splitfield => $self->{splitfield},
-        scope      => $self->{scope},
-        # elide limit, offset
-    );
-
-    # ensure that indexes are accurate on the fields we're not actually using - needed by _do_split and fetch_fieldmap
-    # deliberately overwriting index which was set when $self was created
-    map { $include{$_->name}->_force_index($_->index) if $include{$_->name} } @{$new->fields};
-    return $new;
-}
-
 
 # when doing a historical function calculation query we try to keep down
 # queries by pulling all data for the active range once and then post-filtering
@@ -145,7 +153,7 @@ sub _create_pretender_query {
 # can be non-monotonic.  which means some validity spans are empty while others
 # overlap...
 sub _historical_fetch {
-    my ($self, $orig) = @_;
+    my ($self) = @_;
 
     my $sess = $self->{session};
     my $qc = $sess->query_cache;
@@ -192,8 +200,8 @@ sub _historical_fetch {
     }
 
     @$rows = grep { $_ } @$rows;
-    splice(@$rows, 0, $orig->{offset}) if $orig->{offset};
-    splice(@$rows, $orig->{limit}) if $orig->{limit} && @$rows > $orig->{limit};
+    splice(@$rows, 0, $self->{offset}) if $self->{offset};
+    splice(@$rows, $self->{limit}) if $self->{limit} && @$rows > $self->{limit};
 
     return $rows;
 }
@@ -220,14 +228,13 @@ sub fetch_segment{
 sub _do_split{
       my $self = shift;
 
-    my ($rows, $newquery) = $self->fetch_all_records;
       # Should have a splitfield if we're getting here. Don't check for it. speeed.
       defined( my $idx = $self->{splitfield}->index ) or croak 'field object must provide an index';
 
       my %groupby;
 
       # this was an eval, but there is scant to be gained replacing pp_padsv with pp_const
-      for my $rec (@$rows) {
+      for my $rec ($self->fetch_all_records) {
           push @{$groupby{ $rec->[0][$idx] }}, $rec;
       }
 
@@ -237,14 +244,8 @@ sub _do_split{
 sub fetch_all_records {
     my $self = shift;
 
-    my ($newquery, $sth, $rows) = $self->_exec;
-
-    if (!$rows) {
-        defined( $sth->execute ) or croak 'failed to execute statement (' . $sth->errstr. ')';
-        $rows = $sth->fetchall_arrayref or croak 'failed to execute statement (' . $sth->errstr . ')';
-    }
-
-    my $recobj = $newquery->get_record_obj;
+    my $rows = $self->_exec;
+    my $recobj = $self->get_record_obj;
     my $class = $recobj->class;
 
     my @out;
@@ -255,43 +256,25 @@ sub fetch_all_records {
         map { weaken $_ } @$chunk;
     }
 
-    return wantarray ? (\@out, $newquery) : \@out;
+    return wantarray ? @out : \@out;
 }
 
 sub fetch_column {
     my ($self) = @_;
-    my ($newquery, $sth, $rows) = $self->_exec;
-
-    if ($rows) {
-        my $ix = $self->fields->[0]->index;
-        return [ map($_->[$ix], @$rows) ];
-    }
-
-    $sth->execute;
-
-    my ($val,@list);
-
-    $sth->bind_col(1, \$val) || die "Failed to bind column";
-    push @list, $val while $sth->fetch;
-
-    $sth->finish;
-    return \@list;
+    my $rows = $self->_exec;
+    return [map($_->[0], @$rows)];
 }
 
 sub fetch_fieldmap {
     my ($self, $pk, $field) = @_;
-    my ($newquery, $sth, $rows) = $self->_exec;
-    my $getrow = 'shift(@$rows)';
-
-    if ($sth) {
-        $getrow = '$sth->fetchrow_arrayref()';
-        $sth->execute() or return $self->_error('Failed to execute sth');
-    }
+    confess('should not get here for a historical fetch - all late fetching should be direct on the log') if $self->{is_historical};
+    my $sth = $self->run;
+    $sth->execute() or return $self->_error('Failed to execute sth');
 
     my $lut = {};
 
     my $e = qq{
-        while (my \$row = $getrow) {
+        while (my \$row = \$sth->fetchrow_arrayref) {
             \$lut->${\ join "", map("{\$row->[".$_->index."]}",@$pk) } = \$row->[${\$field->index}];
         }
     };
